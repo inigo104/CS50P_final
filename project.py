9c8fd143134bf64e0f17f9b4c16dd762d47b3314
inigo104 <inigo104@gmail.com>
2023-07-09 15:11:16 +0200

    Add GUI with tkinter

--- project.py
+++ project.py
@@ -1,67 +1,101 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
 from docxtpl import DocxTemplate
 import pandas as pd
 from docx2pdf import convert
 
 CLIENTS_SHEET_NAME = 1
 
-def main():
-    try:
+class InvoiceApp:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Invoice App")
+
+        self.tree = ttk.Treeview(master, selectmode='extended')
+        self.tree["columns"]=("date", "day_of_week", "name", "total", "notes")
+        self.tree.column("#0", width=100, minwidth=50, stretch=tk.NO)
+        self.tree.column("date", width=100, minwidth=50, stretch=tk.NO)
+        self.tree.column("day_of_week", width=100, minwidth=50, stretch=tk.NO)
+        self.tree.column("name", width=200, minwidth=100)
+        self.tree.column("total", width=100, minwidth=50, stretch=tk.NO)
+        self.tree.column("notes", width=400, minwidth=200)
+        
+        self.tree.heading("#0",text="Invoice Number",anchor=tk.W)
+        self.tree.heading("date", text="Date",anchor=tk.W)
+        self.tree.heading("day_of_week", text="Day of Week",anchor=tk.W)
+        self.tree.heading("name", text="Name",anchor=tk.W)
+        self.tree.heading("total", text="Total",anchor=tk.W)
+        self.tree.heading("notes", text="Notes",anchor=tk.W)
+
         excel_path = "invoice_data.xlsx"
-        df_invoice = pd.read_excel(excel_path).set_index('invoice_number')
-        df_client = pd.read_excel(excel_path, sheet_name=CLIENTS_SHEET_NAME).set_index('id')
+        self.df_invoice = pd.read_excel(excel_path).set_index('invoice_number')
+        self.df_client = pd.read_excel(excel_path, sheet_name=CLIENTS_SHEET_NAME).set_index('id')
 
-        invoice_number = choose_invoice(df_invoice)
-        invoice_data = get_invoice(df_invoice, invoice_number)
-        client_data = get_client(df_client, invoice_data["client_id"])
+        self.populate_treeview()
+        self.tree.pack()
 
-        context = create_context(invoice_data, client_data)
+        self.submit_button = tk.Button(master, text="Generate Invoice", command=self.main)
+        self.submit_button.pack()
 
-        doc = DocxTemplate('Invoice_template.docx')
-        doc.render(context)
+    def populate_treeview(self):
+        for invoice_number in self.df_invoice.index:
+            invoice_data = self.df_invoice.loc[invoice_number]
+            total = round(invoice_data["amunt"] * invoice_data["price_per"], 2)
+            self.tree.insert('', 'end', text=str(invoice_number), values=(invoice_data['date'].strftime('%d-%m-%Y'), invoice_data['date'].day_name(), invoice_data['name'], total, invoice_data['notes']))
 
-        filename = f'invoices/{invoice_data.name}.docx'
-        doc.save(filename)
-        convert(filename)
-    except ValueError as e:
-        print(e)
+    def main(self):
+        try:
+            selected_invoices = [self.tree.item(item)['text'] for item in self.tree.selection()]
+            for invoice_number in selected_invoices:
+                invoice_data = self.get_invoice(invoice_number)
+                client_data = self.get_client(invoice_data["client_id"])
 
+                context = self.create_context(invoice_data, client_data)
 
-def choose_invoice(df_invoice: pd.DataFrame) -> str:
-    print(df_invoice[['date','name']].tail(5))
-    return input("Enter invoice number: ").strip()
+                doc = DocxTemplate('invoice_template.docx')
+                doc.render(context)
 
-def get_invoice(df_invoice: pd.DataFrame, invoice_number: str) -> pd.Series:
-    if invoice_number not in df_invoice.index:
-        raise ValueError(f"Invoice number {invoice_number} not found.")
-    return df_invoice.loc[invoice_number]
+                filename = f'invoices/{invoice_data.name}.docx'
+                doc.save(filename)
+                convert(filename)
+            messagebox.showinfo("Success", "Invoice(s) generated successfully!")
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
 
-def get_client(df_client: pd.DataFrame, client_id: str) -> pd.Series:
-    if client_id not in df_client.index:
-        raise ValueError(f"Client id {client_id} not found.")
-    return df_client.loc[client_id]
+    def get_invoice(self, invoice_number: str) -> pd.Series:
+        if invoice_number not in self.df_invoice.index:
+            raise ValueError(f"Invoice number {invoice_number} not found.")
+        return self.df_invoice.loc[invoice_number]
 
-def create_context(invoice_data: pd.Series, client_data: pd.Series) -> dict:
-    formatted_date = invoice_data["date"].date().strftime('%d-%m-%Y')
-    price_per = round(invoice_data["price_per"], 2)
-    price_total = round(invoice_data["amunt"] * invoice_data["price_per"], 2)
-    return {
-        #Invoice data - Customer Data
-        "name": client_data["name"],
-        "identification": client_data["identification"],
-        "address": client_data["addreess"],
-        "postal_code": client_data["postal_code"],
-        "city": client_data["city"],
-        "country": client_data["country"],
-        #Invoice data - Invoice Data
-        "invoice_number": invoice_data.name,
-        "date": formatted_date,
-        "notes": invoice_data["notes"],
-        "item_description": invoice_data["item_description"],
-        "amunt": invoice_data["amunt"],
-        "price_per": price_per,
-        "currency": invoice_data["currency"],
-        "price_total": price_total
-    }
+    def get_client(self, client_id: str) -> pd.Series:
+        if client_id not in self.df_client.index:
+            raise ValueError(f"Client id {client_id} not found.")
+        return self.df_client.loc[client_id]
 
-if __name__ == "__main__":
-    main()
+    def create_context(self, invoice_data: pd.Series, client_data: pd.Series) -> dict:
+        formatted_date = invoice_data["date"].date().strftime('%d-%m-%Y')
+        price_per = round(invoice_data["price_per"], 2)
+        price_total = round(invoice_data["amunt"] * invoice_data["price_per"], 2)
+        return {
+            #Invoice data - Customer Data
+            "name": client_data["name"],
+            "identification": client_data["identification"],
+            "address": client_data["addreess"],
+            "postal_code": client_data["postal_code"],
+            "city": client_data["city"],
+            "country": client_data["country"],
+            #Invoice data - Invoice Data
+            "invoice_number": invoice_data.name,
+            "date": formatted_date,
+            "notes": invoice_data["notes"],
+            "item_description": invoice_data["item_description"],
+            "amunt": invoice_data["amunt"],
+            "price_per": price_per,
+            "currency": invoice_data["currency"],
+            "price_total": price_total
+        }
+
+root = tk.Tk()
+my_gui = InvoiceApp(root)
+root.mainloop()
